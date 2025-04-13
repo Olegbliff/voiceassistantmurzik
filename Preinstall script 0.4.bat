baa67c05e4b14562f4c512d1db3171eb22739adf
OlegT <72335298+Olegbliff@users.noreply.github.com>
2025-04-13 11:16:29 +0300

    Add files via upload

---
+++ Preinstall script 0.4.bat
@@ -0,0 +1,107 @@
+@echo off
+color 1
+echo installing pip package
+
+cls
+color 2
+cls
+pip install PyAudio
+cls
+color 3
+cls
+pip install SpeechRecognition
+cls
+color 4
+cls
+pip install google-generativeai
+cls
+color 5
+cls
+pip install openai
+cls
+color 6
+cls
+pip install webbrowser_open
+cls
+color 7
+cls
+cd C:\Py\
+cls
+color 12
+pip install pygetwindow
+cls
+pip install psutil
+cls
+color 2
+cls
+color 1
+pip install python-docx
+cls
+pip install pywin32
+color 2
+pip install psutil google-generativeai SpeechRecognition pygetwindow requests winshell pyautogui Pillow comtypes pycaw pywin32
+cls
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+
+echo Uninstalled successfullyecho wscript.Sleep 3000>"%temp%\sleep3.vbs"
+cscript //nologo "%temp%\sleep3.vbs"
+del "%temp%\sleep3.vbs"

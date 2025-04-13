1c1c45d289d67b9151498d9c79f80d4a4af09a1e
OlegT <72335298+Olegbliff@users.noreply.github.com>
2025-04-13 12:49:06 +0300

    Add files via upload

---
+++ Uninstall script 0.4.bat
@@ -0,0 +1,104 @@
+@echo off
+color 1
+echo uninstalling pip package
+
+cls
+color 2
+cls
+pip uninstall PyAudio
+cls
+color 3
+cls
+pip uninstall SpeechRecognition
+cls
+color 4
+cls
+pip uninstall google-generativeai
+cls
+color 5
+cls
+pip uninstall openai
+cls
+color 6
+cls
+pip uninstall webbrowser_open
+cls
+color 7
+cls
+cd C:\Py\
+cls
+color 12
+pip uninstall pygetwindow
+cls
+pip uninstall psutil
+cls
+color 2
+cls
+color 1
+pip uninstall python-docx
+cls
+pip uninstall pywin32
+color 2
+pip uninstall psutil google-generativeai SpeechRecognition pygetwindow requests winshell pyautogui Pillow comtypes pycaw pywin32
+cls
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+echo successfully
+
